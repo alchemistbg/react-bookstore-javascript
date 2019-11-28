c3a156a208bcc89f9baa8a9d3cf1db2a1891ae4b
alchemistbg <alchemist_bg@abv.bg>
2019-11-28 17:45:29 +0200

    Add 'user' controller
    Functions to be implemented

---
+++ back-end/controllers/users.js
@@ -0,0 +1,24 @@
+const userModel = require('../models/user');
+
+module.exports = {
+
+    register: (req, res) => {
+    },
+
+    login: (req, res) => {
+
+    },
+
+    profileRead: (req, res) => {
+
+    },
+
+    profileEdit: (req, res) => {
+
+    },
+
+    profileDelete: (req, res) => {
+
+    },
+
+}
