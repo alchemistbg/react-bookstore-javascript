407023240a36acf35ccaeaefb9aaee7d5ced0f93
alchemistbg <alchemist_bg@abv.bg>
2019-11-28 17:14:27 +0200

    Add books Router to index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const port = 3000;
 const app = express();
 
+require('./config/database')();
 
+const booksRouter = require('./routes/books');
+
+app.use(bodyParser.json());
+
+app.use('/routes/books', booksRouter);
 
 app.listen(port, () => {
     console.log('App listening on port 3000!');
