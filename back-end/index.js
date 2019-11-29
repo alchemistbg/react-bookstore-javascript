1f87646e98f39d984bcdaf6dbc4f42f1da17d5fb
alchemistbg <alchemist_bg@abv.bg>
2019-11-29 23:50:59 +0200

    Update index file

--- back-end/index.js
+++ back-end/index.js
@@ -6,10 +6,20 @@ const app = express();
 require('./config/database')();
 
 const booksRouter = require('./routes/books');
+const usersRouter = require('./routes/users');
 
 app.use(bodyParser.json());
 
 app.use('/routes/books', booksRouter);
+app.use('/routes/users', usersRouter);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+    next();
+})
 
 app.listen(port, () => {
     console.log('App listening on port 3000!');
