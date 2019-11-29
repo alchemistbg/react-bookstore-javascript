27ae57ada51486fac7114207ad34b0fc372e6d65
alchemistbg <alchemist_bg@abv.bg>
2019-11-29 23:43:49 +0200

    Update 'user' controller
    Implement 'register'
    
    Implement 'login'

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -1,12 +1,98 @@
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+
 const userModel = require('../models/user');
+
+function validateUserInfo(req, res) {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(422).json({
+            message: 'Incorrect user data',
+            errors: errors.array()
+        });
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = {
 
     register: (req, res) => {
+
+        if (validateUserInfo(req, res)) {
+            const { username, password, email, userRole } = req.body;
+            userModel.create({ username, password, email, userRole })
+                .then((user) => {
+                    res.status(200).json({
+                        message: 'User registered successfully',
+                        userId: user._id
+                    });
+                })
+                .catch((error) => {
+                    if (!error.statusCode) {
+                        error.statusCode = 500;
+                    }
+                    next(error);
+                    // error = new Error('Internal server error!');
+                    // error.statusCode = 500;
+                    // throw error;
+                    // next(error);
+                });
+        }
     },
 
-    login: (req, res) => {
+    login: (req, res, next) => {
+        const { username, password } = req.body;
 
+        userModel.findOne({ username })
+            //to used with promise-based password check
+            // .then((user) => {
+            //     user.matchPassword(password)
+            //         .then((match) => {
+            //             if (!match) {
+            //                 const error = new Error('Wrong password');
+            //                 error.statusCode = 401;
+            //                 throw error;
+            //             }
+            //             console.log(match);
+            //         })
+            //         .catch((error) => {
+            //             next(error);
+            //         });
+            // }
+
+            ////to used with async-based password check
+            .then(async (user) => {
+                if (!user) {
+                    const error = new Error('User doesn\'t exist');
+                    error.statusCode = 401;
+                    throw error;
+                }
+
+                if (! await user.matchPassword(password)) {
+                    const error = new Error('Wrong password');
+                    error.statusCode = 401;
+                    throw error;
+                }
+
+                const token = jwt.sign({
+                }, 'verysecretstring', { expiresIn: '1h' });
+
+                res.status(200).json({
+                    message: 'User logged in successfully',
+                    token,
+                    userId: user._id.toString(),
+                    username: user.username,
+                    userRole: user.userRole
+                });
+                console.log(token);
+            })
+            .catch((error) => {
+                next(error);
+            });
     },
 
     profileRead: (req, res) => {
