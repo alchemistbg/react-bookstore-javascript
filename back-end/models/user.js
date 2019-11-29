74dd61c7e7146e1a83ad460d8791ff721de23e13
alchemistbg <alchemist_bg@abv.bg>
2019-11-29 23:46:57 +0200

    Update 'user' model
    Implement model schema
    
    Implement matchPassword method
    
    Implement pre('save') hook

---
+++ back-end/models/user.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    userRole: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+        // required: true
+    }
+});
+
+userSchema.methods = {
+    //async-based version
+    matchPassword: async function (password) {
+        return await bcrypt.compare(password, this.password);
+    }
+
+    //promise-based
+    // matchPassword: function (password) {
+    //     return bcrypt.compare(password, this.password);
+    // }
+};
+
+//async-based pre-save
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        try {
+            const salt = await bcrypt.genSalt(saltRounds);
+            const hashedPassword = await bcrypt.hash(this.password, salt);
+            this.password = hashedPassword;
+            // next();
+        } catch (error) {
+            //TODO
+        }
+    }
+    next();
+});
+
+//promise-based pre-save
+// userSchema.pre('save', function (next) {
+//     if (this.isModified('password')) {
+//         bcrypt.genSalt(saltRounds, (err, salt) => {
+//             if (err) {
+//                 next(err);
+//                 return;
+//             }
+//             bcrypt.hash(this.password, salt, (err, hash) => {
+//                 if (err) {
+//                     next(err);
+//                     return;
+//                 }
+//                 this.password = hash;
+//                 next();
+//             });
+//         });
+//         return;
+//     }
+//     next();
+// });
+
+module.exports = mongoose.model('user', userSchema);
