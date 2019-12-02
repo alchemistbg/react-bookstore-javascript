7a43524402031bb99095a17949127ada94139778
alchemistbg <alchemist_bg@abv.bg>
2019-12-02 13:12:39 +0200

    Updste 'users' router
    Add read user profile

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const utils = require('../utils')
 
-
 const userController = require('../controllers/users');
 
 router.post('/', utils.validator.validateRegistrationData(), userController.register);
 router.post('/me', userController.login);
+
+router.get('/:id', userController.profileRead);
 
 module.exports = router;
