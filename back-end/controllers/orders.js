18bbdcaa216f916ed0e92f7f7d27b6ff8a6c3bca
alchemistbg <alchemist_bg@abv.bg>
2019-12-02 13:44:00 +0200

    Add initial version of 'orders' contoller
    Add implementation of 'orderAdd' method
    
    Add implementation of getOrders and 'getOrderById' methods

---
+++ back-end/controllers/orders.js
@@ -0,0 +1,57 @@
+const orderModel = require('../models/order');
+const userModel = require('../models/user');
+
+const mongoose = require('mongoose');
+
+module.exports = {
+
+    orderAdd: (req, res) => {
+        const userId = req.body.userId;
+
+        const customer = mongoose.mongo.ObjectId(userId);
+        orderModel.create({ customer })
+            .then((order) => {
+
+                userModel.findById(userId)
+                    .populate({ path: 'order' })
+                    .select('username orders')
+                    .then((user) => {
+
+                        user.orders.push(order._id);
+                        user.save();
+
+                        res.status(201).json({
+                            message: "Order created",
+                            order,
+                            user
+                        })
+                    })
+                // .catch();
+            })
+            .catch();
+    },
+
+    getOrders: (req, res) => {
+        orderModel.find()
+            .populate({ path: 'customer', select: '_id' })
+            .then((orders) => {
+                res
+                    .status(200)
+                    .json({
+                        orders
+                    });
+            })
+            .catch();
+    },
+
+    getOrderById: (req, res) => {
+        const orderId = req.params.id;
+
+        orderModel.findById(orderId)
+            .then((order) => {
+                console.log(order)
+            })
+            .catch();
+
+    }
+}
