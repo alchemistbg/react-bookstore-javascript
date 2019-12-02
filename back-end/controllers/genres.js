e0e2948e30cdc2170eeb8c8bf22befd19b33bb4a
alchemistbg <alchemist_bg@abv.bg>
2019-12-02 13:46:57 +0200

    Add initial version of 'genres' contoller
    Add implementation of 'genreAdd' method
    
    Add implementation of 'getGenres'

---
+++ back-end/controllers/genres.js
@@ -0,0 +1,31 @@
+const genreModel = require('../models/genre');
+
+module.exports = {
+
+    getGenres: (req, res) => {
+        genreModel.find()
+            .then((genres) => {
+                console.log(genres);
+            })
+            .catch();
+    },
+
+    genreAdd: (req, res) => {
+        // console.log(req.body)
+        const { genreName } = req.body;
+        const genre = new genreModel({ name: genreName });
+        genre.save();
+    },
+
+    // genreDetails: () => {
+
+    // },
+
+    // genreEdit: () => {
+
+    // },
+    // genreDelete: () => {
+
+    // },
+
+}
