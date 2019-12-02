b4fc88c978ae89edafac5229a060f37d4f0d91b4
alchemistbg <alchemist_bg@abv.bg>
2019-12-02 13:50:32 +0200

    Add 'orders' router

---
+++ back-end/routes/orders.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const orderController = require('../controllers/orders');
+
+router.post('/', orderController.orderAdd);
+
+router.get('/', orderController.getOrders);
+router.get('/:id', orderController.getOrderById);
+
+module.exports = router
