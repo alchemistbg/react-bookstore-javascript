a9920faac44adbacc211b71d0ca9121afbdca30e
alchemistbg <alchemist_bg@abv.bg>
2019-12-02 13:40:26 +0200

    Add initial version of 'order' model

--- back-end/models/order.js
+++ back-end/models/order.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
 
 });
 
