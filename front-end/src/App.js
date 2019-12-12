337b0f9ed10efb159daced8e6fec000955d9c330
alchemistbg <alchemist_bg@abv.bg>
2019-12-12 16:21:58 +0200

    Update 'App.js' and 'App.css' files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Navigation/Header';
+import Footer from './components/Navigation/Footer';
+
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+
+import About from './components/About';
+import Contacts from './components/Contacts';
+import LoginForm from './components/Forms/LoginForm';
+import RegisterForm from './components/Forms/RegisterForm';
+import NotFound from './components/NotFound';
+
+class App extends Component {
+
+	render() {
+		return (
+			document.title = "Reactive Bookstore",
+			<div className='site-container'>
+				<Router>
+					<Header />
+					<main className='site-main'>
+						<Switch>
+							<Route exact path="/" component={BookList} />
+							<Route path="/about" component={About} />
+							<Route path="/contacts" component={Contacts} />
+							<Route path="/login" component={LoginForm} />
+							<Route path="/register" component={RegisterForm} />
+							<Route path="/books/:id" component={BookDetails} />
+
+							<Route path="/not-found" component={NotFound} />
+							<Redirect to="/not-found" />
+						</Switch>
+					</main>
+					<Footer />
+				</Router>
+			</div >
+		);
+	}
 }
 
 export default App;
