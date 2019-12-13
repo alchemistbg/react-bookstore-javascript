5212bc1c71b49df91eaea50ddff0f6137c95dee2
alchemistbg <alchemist_bg@abv.bg>
2019-12-13 17:32:59 +0200

    Update 'Donut' component
    Clear the code

--- front-end/src/components/Donut.jsx
+++ front-end/src/components/Donut.jsx
@@ -4,14 +4,10 @@ function Donut() {
     return (
         <div className="donut-wrapper">
             <p className="donut-text">Loading...</p>
-            <div className="donut donut.odd">
+            <div className="donut donut-odd">
                 <div className="donut">
-                    <div className="donut donut.odd">
+                    <div className="donut donut-odd">
                         <div className="donut">
-                            {/* <div className={`${donut.donut} ${donut.odd}`}> */}
-                            {/* <div className={donut.donut}>
-                                </div> */}
-                            {/* </div> */}
                         </div>
                     </div>
                 </div>
