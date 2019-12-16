597425b072cf6934561c4c5a65907b63d7a885db
alchemistbg <alchemist_bg@abv.bg>
2019-12-16 11:36:49 +0200

    Update 'books' routes
    Add routes for categories
    
    Add routes for genres

--- back-end/routes/books.js
+++ back-end/routes/books.js
@@ -4,10 +4,18 @@ const router = express.Router();
 const bookController = require('../controllers/books');
 
 router.get('/', bookController.getBooks);
+router.get('/upcoming', bookController.getUpcomingBooks);
+router.get('/newest', bookController.getNewestBooks);
+router.get('/bestselling', bookController.getBestsellingBooks);
 
 router.post('/', bookController.bookCreate);
 router.get('/:id', bookController.bookDetails);
 router.put('/:id', bookController.bookUpdate);
 router.delete('/:id', bookController.bookDelete);
+
+router.get('/genres/:id', bookController.getBooksByGenre);
+
+router.get('/:id/comments', bookController.bookGetComments);
+router.post('/:id/comments', bookController.bookPostComment);
 
 module.exports = router;
