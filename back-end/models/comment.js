66b89c9261844af60a0ba4b76162c60aa23a6069
alchemistbg <alchemist_bg@abv.bg>
2019-12-16 11:38:49 +0200

    Update 'comment' model

--- back-end/models/comment.js
+++ back-end/models/comment.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
+    commentCreator: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    commentTime: {
+        type: Date,
+        default: Date.now
+    },
+    commentContent: {
+        type: String,
+        required: true
+    }
 
 });
 
