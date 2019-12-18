a4f4ba2ff649e1ee1de34045a5ad2b4ef1a1e75b
alchemistbg <alchemist_bg@abv.bg>
2019-12-18 21:00:22 +0200

    Update 'Login' form

--- front-end/src/components/Forms/LoginForm.jsx
+++ front-end/src/components/Forms/LoginForm.jsx
@@ -2,7 +2,9 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 import Input from './Input';
-import { loginUser } from '../../utils/requests';
+import { loginUser } from '../../services/requests';
+import { validateForm, validateInput } from '../../utils/inputValidation';
+import { showToast } from '../../utils/helpers';
 
 class LoginForm extends Component {
     constructor(props) {
@@ -10,24 +12,19 @@ class LoginForm extends Component {
 
         this.state = {
             pageName: 'Reactive Bookstore | Login',
+            editing: undefined,
             loginInfo: {
                 username: '',
                 password: ''
             },
             loginErrors: {
 
+            },
+            inputError: {
+                username: false,
+                password: false,
             }
         }
-    }
-
-    handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const user = await loginUser(this.state.loginInfo);
-        } catch (error) {
-            console.log(error.response.data.param)
-        }
-        // console.log(event.target)
     }
 
     handleChange = ({ currentTarget: input }) => {
@@ -44,8 +41,78 @@ class LoginForm extends Component {
         });
     }
 
+    handleSubmit = (event) => {
+        const { loginInfo } = this.state;
+        event.preventDefault();
+        validateForm('login', loginInfo)
+            .then(() => {
+                loginUser(this.state.loginInfo)
+                    .then((response) => {
+                        console.log(response)
+                        showToast('success', {
+                            title: `Welcome, ${response.data.username}.`,
+                            message: `You will be now redirected to home page.`
+                        });
+                    })
+                    .catch((error) => {
+                        const { param, message } = error.response.data;
+                        const serverError = {};
+                        serverError[param] = [message];
+                        showToast('error', serverError);
+                    });
+            })
+            .catch((errors) => {
+
+                this.setState({
+                    loginErrors: errors
+                });
+                showToast('error', this.state.loginErrors)
+            });
+    }
+
+    handleFocus = () => {
+        // console.log('FUCK FOCUS');
+        // this.setState({
+        //     editing: true
+        // });
+    }
+
+    handleBlur = ({ currentTarget: input }) => {
+        validateInput('login', input.name, this.state.loginInfo)
+            .then((errors) => {
+                console.log(errors);
+                this.setState({
+                    inputError: {
+                        username: (!!errors.username),
+                        password: (!!errors.password)
+                    }
+                    // usernameError: !(!!errors.username),
+                    // passwordError: !(!!errors.password)
+                });
+            })
+            .catch(errors => {
+                console.log(errors)
+                if (errors.username) {
+                    this.setState({
+                        inputError: {
+                            username: (!!errors.username)
+                        }
+                    });
+                }
+                if (errors.password) {
+                    this.setState({
+                        inputError: {
+                            password: (!!errors.password)
+                        }
+                    });
+                }
+            });
+    }
+
     render() {
         const { pageName, loginInfo } = this.state;
+        const { username, password } = this.state.inputError;
+        // console.log(username, password);
         return (
             document.title = pageName,
             < Fragment >
@@ -53,24 +120,48 @@ class LoginForm extends Component {
                 <div id="login-form">
                     <form className="form login-form" onSubmit={this.handleSubmit}>
                         <Input
-                            isAutoFocus={true}
-                            divClassNames="form-field-wrapper uname-wrapper"
+                            divClassNames={
+                                username ? (
+                                    "form-field-wrapper uname-wrapper danger"
+                                ) : (
+                                        "form-field-wrapper uname-wrapper"
+                                    )
+                            }
+                            isAutoFocus={false}
                             value={loginInfo.username}
+                            onFocus={this.handleFocus}
                             onChange={this.handleChange}
+                            onBlur={this.handleBlur}
                             type="text"
                             nameAndId="username"
                             labelClassName="placeholder"
-                            labelTextValue="Username" />
+                            labelTextValue="Username"
+                            error="no errors" />
                         <Input
+                            divClassNames={
+                                password ? (
+                                    "form-field-wrapper pass-rep-wrapper danger"
+                                ) : (
+                                        "form-field-wrapper pass-rep-wrapper"
+                                    )
+                            }
                             isAutoFocus={false}
-                            divClassNames="form-field-wrapper pass-rep-wrapper"
                             value={loginInfo.password}
                             onChange={this.handleChange}
+                            onChange={this.handleChange}
+                            onBlur={this.handleBlur}
                             type="password"
                             nameAndId="password"
                             labelClassName="placeholder"
-                            labelTextValue="Password" />
-                        <button className="form-button" type="submit">Login</button>
+                            labelTextValue="Password"
+                            error="no errors" />
+                        {
+                            !!(username || password) ? (
+                                <button className="form-button" disabled={true} type="submit">Login</button>
+                            ) : (
+                                    < button className="form-button" type="submit">Login</button>
+                                )
+                        }
                     </form>
                 </div>
                 <div>Don't have an account? Please go to {<Link to="/register">Register</Link>} page.</div>
