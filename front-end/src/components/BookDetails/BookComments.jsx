90328d25b20ace04b96bb6b1c2f029a50ed30e9f
alchemistbg <alchemist_bg@abv.bg>
2020-01-09 11:59:23 +0200

    Refactor 'BookComment' component into functional component

--- front-end/src/components/BookDetails/BookComments.jsx
+++ front-end/src/components/BookDetails/BookComments.jsx
@@ -4,77 +4,57 @@ import { timeFormat } from '../../utils/helpers';
 import Pagination from './../Common/Pagination/Pagination';
 import { paginate } from './../../utils/paginate';
 
-class BookComments extends Component {
-    constructor(props) {
-        super(props);
+function BookComments(props) {
+    const { book } = props;
+    const { length: commentsCount } = book.comments;
+    const { comments: allComments } = book;
 
-        this.state = {
-            pageSize: 5,
-            currentPage: 1
-        }
+    const [pageSize, setPageSize] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const commentsPage = paginate(allComments, currentPage, pageSize);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
     }
 
-    handlePageChange = (page) => {
-        this.setState({
-            currentPage: page
-        });
-    }
+    return (
+        <Fragment>
+            <h5>Comments:</h5>
+            {
+                commentsCount === 0 ?
+                    (
+                        <h6>There are no comments yet</h6>
+                    ) : (
+                        <ul className="comments-list">
+                            {
+                                commentsPage.map((comment) => {
+                                    const commentTime = timeFormat(comment.commentTime);
+                                    const content = comment.commentContent;
+                                    const userName = comment.commentCreator.userName;
 
-    render() {
-        const { book } = this.props;
-        const { length: commentsCount } = book.comments;
-        const { comments: allComments } = book;
-        const { pageSize, currentPage } = this.state;
-
-        const commentsPage = paginate(allComments, currentPage, pageSize);
-
-        return (
-            <Fragment>
-                <h5>Comments:</h5>
-                {
-                    commentsCount === 0 ?
-                        (
-                            <h6>There are no comments yet</h6>
-                        ) : (
-                            <ul className="comments-list">
-                                {
-                                    commentsPage.map((comment) => {
-                                        const commentTime = timeFormat(comment.commentTime);
-                                        const content = comment.commentContent;
-                                        const userName = comment.commentCreator.userName;
-
-                                        return <li key={comment.commentTime} className="comment-item">
-                                            <div className="comment-meta">
-                                                <span className="comment-time">on {commentTime} </span>
-                                                <span className="comment-author">{userName} wrote:</span>
-                                            </div>
-                                            <div className="comment-content">
-                                                {content}
-                                            </div>
-                                        </li>
-                                    })
-                                }
-                            </ul>
-                        )
-                }
-                <Pagination
-                    link={`/books/${book._id}`}
-                    itemsCount={allComments.length}
-                    pageSize={pageSize}
-                    currentPage={currentPage}
-                    onPageChange={this.handlePageChange} />
-            </Fragment>
-        );
-    }
+                                    return <li key={comment.commentTime} className="comment-item">
+                                        <div className="comment-meta">
+                                            <span className="comment-time">on {commentTime} </span>
+                                            <span className="comment-author">{userName} wrote:</span>
+                                        </div>
+                                        <div className="comment-content">
+                                            {content}
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    )
+            }
+            <Pagination
+                link={`/books/${book._id}`}
+                itemsCount={allComments.length}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange} />
+        </Fragment>
+    );
 }
-
-// export default BookComments;
-// function BookComments(props) {
-//     const { book } = props;
-
-//     return (
-
-//     );
-// }
 
 export default BookComments;
