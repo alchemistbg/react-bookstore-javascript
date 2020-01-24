16e7f8db4858d2dc32d52f66bdd979cf240e9492
alchemistbg <alchemist_bg@abv.bg>
2020-01-24 12:08:18 +0200

    Update file
    Rename context provider and consumer

--- front-end/src/context/authContext/AuthContext.jsx
+++ front-end/src/context/authContext/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthContext = React.createContext({});
 
-export const Provider = AuthContext.Provider;
-export const Consumer = AuthContext.Consumer;
+export const AuthProvider = AuthContext.Provider;
+export const AuthConsumer = AuthContext.Consumer;
 
 export default AuthContext;
