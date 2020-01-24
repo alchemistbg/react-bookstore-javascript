eee036d57f370dd30d8138944c728cd9abd86893
alchemistbg <alchemist_bg@abv.bg>
2020-01-24 12:12:41 +0200

    Update cart reducer
    Fix ADD_TO_CART action
    
    Fix REMOVE_FROM_CART action
    
    Add and implement CHECKOUT action
    
    Add and implement INCREMENT action
    
    Add and implement DECREMENT action

--- front-end/src/reducers/cartReducer.jsx
+++ front-end/src/reducers/cartReducer.jsx
@@ -1,17 +1,53 @@
-export const initialCartState = { cart: [] };
-
-export const cartReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TO_CART':
-            return {
-                cart: state.cart.concat(action.item)
-            }
-        case 'REMOVE_FROM_CART':
-            return {
-                cart: state.cart.filter(item => item.id !== action.id)
-            }
-        default:
-            return state
-    }
+export const initialCartState = {
+    cart: []
 };
 
+export const cartReducer = (state, action) => {
+    let itemIndex;
+    if (action.item) {
+        itemIndex = state.cart.findIndex(item => item._id === action.item._id) || 0;
+    }
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            if (itemIndex > -1) {
+                state.cart[itemIndex].qty += 1;
+                state.cart[itemIndex].totalPrice = (state.cart[itemIndex].qty * state.cart[itemIndex].price).toFixed(2);
+                return {
+                    cart: state.cart
+                }
+            } else {
+                action.item.qty = 1;
+                action.item.totalPrice = action.item.price.toFixed(2);
+                return {
+                    cart: state.cart.concat(action.item)
+                }
+            }
+
+        case 'INCREMENT':
+            state.cart[itemIndex].qty += 1
+            state.cart[itemIndex].totalPrice = (state.cart[itemIndex].qty * state.cart[itemIndex].price).toFixed(2);
+            return {
+                cart: state.cart
+            }
+
+        case 'DECREMENT':
+            if (state.cart[itemIndex].qty > 1) {
+                state.cart[itemIndex].qty -= 1;
+                state.cart[itemIndex].totalPrice = (state.cart[itemIndex].qty * state.cart[itemIndex].price).toFixed(2);
+            }
+            return {
+                cart: state.cart
+            }
+
+        case 'REMOVE_FROM_CART':
+            return {
+                cart: state.cart.filter((item) => item._id !== action.item._id)
+            }
+
+        case 'CHECKOUT':
+            return { cart: [] };
+
+        default:
+            return state;
+    }
+}
