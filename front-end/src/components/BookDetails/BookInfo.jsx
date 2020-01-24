3b797b26f1c8438e830229a530ef563465841e65
alchemistbg <alchemist_bg@abv.bg>
2020-01-24 23:29:34 +0200

    Finish Add-to-cart button
    Add table for vizualizing book information

--- front-end/src/components/BookDetails/BookInfo.jsx
+++ front-end/src/components/BookDetails/BookInfo.jsx
@@ -1,31 +1,84 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
+// import { Redirect } from 'react-router-dom';
+import { showToast } from '../../utils/helpers';
 
-function handleAddToCartClick() {
-    console.log('clicked')
-}
+import AuthContext from './../../context/authContext/AuthContext';
+
+import CartContext from './../../context/cartContext/CartContext';
+// import { initialCartState, cartReducer } from '../../reducers/cartReducer';
 
 function BookInfo(props) {
+    const [{ isLoggedIn }] = useContext(AuthContext);
+
+    const [{ cart }, dispatch] = useContext(CartContext);
+
+    const handleAddToCartClick = () => {
+        if (!isLoggedIn) {
+            props.history.push('/login');
+        } else {
+            showToast('success', {
+                title: `${props.book.title} added successfully to your cart.`
+            });
+            dispatch({
+                type: 'ADD_TO_CART',
+                item: props.book
+            });
+        }
+    }
+
     const { book } = props;
     return (
         <div className="book-info">
             <h4>{book.title}</h4>
-            <div>from {book.author}</div>
-            <div>published by {book.publisher}</div>
-            <div>Categories: {
-                book.genres.map((genre, index) => {
-                    if (index < book.genres.length - 1) {
-                        return <span key={genre._id}>{genre.name}, </span>;
-                    }
-                    return <span key={genre._id}>{genre.name}</span>;
-                })
-            }</div>
-            <div>ISBN: {book.isbn}</div>
-            <div>Price: {book.price}</div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td className="column-header">Author:</td>
+                        <td className="column-data">{book.author}</td>
+                    </tr>
+                    <tr>
+                        <td className="column-header">Publisher:</td>
+                        <td className="column-data">{book.publisher}</td>
+                    </tr>
+                    <tr>
+                        <td className="column-header">Categories: </td>
+                        <td className="column-data">
+
+                            {
+                                book.genres.map((genre, index) => {
+                                    if (index < book.genres.length - 1) {
+                                        return <span key={genre._id}>{genre.name}, </span>;
+                                    }
+                                    return <span key={genre._id}>{genre.name}</span>;
+                                })
+                            }
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="column-header">ISBN:</td>
+                        <td className="column-data">{book.isbn}</td>
+                    </tr>
+                    <tr>
+                        <td className="column-header">Price:</td>
+                        <td className="column-data">{book.price}$</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <span className="qty-wrapper">
+                    <button className="form-button e">
+                        <i className="far fa-minus-square"></i>
+                    </button>
+                    1
+                    <button className="form-button e">
+                        <i className="far fa-plus-square"></i>
+                    </button>
+                </span>
+            </div>
             <input className="form-button" type="button" value="Add to cart" onClick={handleAddToCartClick} />
         </div>
     );
 }
-
-
 
 export default BookInfo;
