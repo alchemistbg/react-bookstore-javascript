e1c39ee783a137c2a6f5b3e0978522edd8358778
alchemistbg <alchemist_bg@abv.bg>
2021-05-04 17:18:56 +0200

    Add 'Quantity' component and its styling

---
+++ front-end/src/components/Common/Quantity/Quantity.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Quantity = (props) => {
+
+    return <span className="book-list-item-qty">
+        {
+            props.book.qty < 2 ? (
+                <button className="form-button book-table" disabled>
+                    <i className="far fa-minus-square"></i>
+                </button>
+            ) : (
+                    <button className="form-button book-table" onClick={() => props.handleDecrement(props.book)}>
+                        <i className="far fa-minus-square"></i>
+                    </button>
+                )
+        }
+        {props.book.qty}
+        <button className="form-button book-table" onClick={() => props.handleIncrement(props.book)}>
+            <i className="far fa-plus-square"></i>
+        </button>
+    </span>
+}
+
+export default Quantity;
