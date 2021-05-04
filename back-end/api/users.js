a8baf4c9961cd58cc7a4797693744ca68fafe258
alchemistbg <alchemist_bg@abv.bg>
2021-05-04 17:08:14 +0200

    Rename routes folder from 'routes' to 'api'

--- back-end/api/users.js
+++ back-end/api/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const utils = require('../utils')
+const utils = require('../utils');
 
 const userController = require('../controllers/users');
 
 router.post('/', utils.validator.registrationDataValidator(), userController.register);
 router.post('/auth', utils.validator.loginDataValidator(), userController.login);
 
-router.get('/:id', userController.profileRead);
+router.get('/:id', utils.auth, userController.profileRead);
+router.patch('/:id', utils.auth, userController.profileEdit);
 
 router.get('/:id/orders', userController.getOrders);
 
