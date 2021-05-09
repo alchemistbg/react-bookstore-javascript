24639e3c769d9136da0f3fc6b483e24c73fa85ce
alchemistbg <alchemist_bg@abv.bg>
2021-05-09 08:16:34 +0200

    Update 'books' controller
    Protect 'bookDelete' method from unauthorized requests

--- back-end/controllers/books.js
+++ back-end/controllers/books.js
@@ -153,8 +153,14 @@ module.exports = {
         }
     },
 
-    bookDelete: (req, res) => {
+    bookDelete: (req, res, next) => {
+        if (req.user.userRole !== "admin") {
+            return res.status(403).json({
+                message: "Forbidden! You do not have rights for this operation."
+            });
+        } else {
 
+        }
     },
 
     bookGetComments: (req, res) => {
