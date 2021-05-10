14911e250d16312a57822c3d5fb1fc17d1ffb403
alchemistbg <alchemist_bg@abv.bg>
2021-05-10 09:19:57 +0200

    Update books router
    Protect some routes only for registered users
    
    These routes accept POST, PATCH and DELETE requests

--- back-end/api/books.js
+++ back-end/api/books.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const utils = require('./../utils');
+
 const bookController = require('../controllers/books');
 
 router.get('/', bookController.getBooks);
@@ -8,14 +10,14 @@ router.get('/upcoming', bookController.getUpcomingBooks);
 router.get('/newest', bookController.getNewestBooks);
 router.get('/bestselling', bookController.getBestsellingBooks);
 
-router.post('/', bookController.bookCreate);
+router.post('/', utils.auth, bookController.bookCreate);
 router.get('/:id', bookController.bookDetails);
-router.put('/:id', bookController.bookUpdate);
-router.delete('/:id', bookController.bookDelete);
+router.patch('/:id', utils.auth, bookController.bookUpdate);
+router.delete('/:id', utils.auth, bookController.bookDelete);
 
 router.get('/genres/:id', bookController.getBooksByGenre);
 
 router.get('/:id/comments', bookController.bookGetComments);
-router.post('/:id/comments', bookController.bookPostComment);
+router.post('/:id/comments', utils.auth, bookController.bookPostComment);
 
 module.exports = router;
