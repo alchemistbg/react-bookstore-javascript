c8a1db60c0616a27e8a49d3c006bf5a18c52771d
alchemistbg <alchemist_bg@abv.bg>
2021-05-12 22:19:09 +0200

    Update 'Order' model
    Add rest of the code
    
    Experience some issue from file renaming

--- back-end/models/order.js
+++ back-end/models/order.js
@@ -4,18 +4,25 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
     customer: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        get: v => v.toString()
     },
     orderedBooks: [{
         bookId: {
             type: Schema.Types.ObjectId,
             ref: 'Book',
+            get: v => v.toString()
         },
         qty: Number,
         totalPrice: Number
     }],
     totalPrice: {
         type: Number
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Approved', 'Processing', 'Packed', 'Sent', 'Canceled', 'Completed'],
+        default: 'Pending'
     },
     orderDate: {
         type: Date,
