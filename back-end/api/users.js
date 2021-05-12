a7868dc26fb7d8d4e597ef96bf3ab7d796a19812
alchemistbg <alchemist_bg@abv.bg>
2021-05-12 22:05:37 +0200

    Update 'users' router
    Rename methods to correspond better with HTTP verbs

--- back-end/api/users.js
+++ back-end/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const utils = require('../utils');
 
 const userController = require('../controllers/users');
@@ -7,9 +8,9 @@ const userController = require('../controllers/users');
 router.post('/', utils.validator.registrationDataValidator(), userController.register);
 router.post('/auth', utils.validator.loginDataValidator(), userController.login);
 
-router.get('/:id', utils.auth, userController.profileRead);
-router.patch('/:id', utils.auth, userController.profileEdit);
+router.get('/:id', utils.auth, userController.getProfile);
+router.patch('/:id', utils.auth, userController.patchProfile);
 
-router.get('/:id/orders', userController.getOrders);
+router.get('/:id/orders', utils.auth, userController.getUserOrders);
 
 module.exports = router;
