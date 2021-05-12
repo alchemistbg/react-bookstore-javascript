cb668fa34b6314e529c13cef818f8b0295ac5e5e
alchemistbg <alchemist_bg@abv.bg>
2021-05-12 22:17:35 +0200

    Update 'Order' model
    Add enum with status of the order

--- back-end/models/Order.js
+++ back-end/models/Order.js
@@ -1,1 +1,6 @@
         get: v => v.toString()
+    status: {
+        type: String,
+        enum: ['Pending', 'Approved', 'Processing', 'Packed', 'Sent', 'Canceled', 'Completed'],
+        default: 'Pending'
+    },
