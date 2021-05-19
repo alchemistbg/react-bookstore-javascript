62cbdbe94a551e0ec31879bfbf5a4bc335dcdfc7
alchemistbg <alchemist_bg@abv.bg>
2021-05-19 20:53:03 +0200

    Update 'index' file
    Import newly installed packages
    
    Perform some code reorganization

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,21 @@
+const dotenv = require('dotenv').config({
+    path: 'envs/.env_dev'
+});
+
 const express = require('express');
+
+const helmet = require('helmet');
+const port = process.env.PORT || 3000;
+
+const cookieParser = require('cookie-parser');
 // const bodyParser = require('body-parser');
-const port = 5001;
+// const cors = require('cors');
+
 const app = express();
+
+app.use(cookieParser(process.env.CP_SECRET));
+app.use(express.json());
+app.use(helmet());
 
 require('./config/database')();
 
@@ -9,24 +23,31 @@ const genresRouter = require('./api/genres');
 const booksRouter = require('./api/books');
 const usersRouter = require('./api/users');
 const ordersRouter = require('./api/orders');
+const commentsRouter = require('./api/comments');
 
 // app.use(bodyParser.json());
-app.use(express.json());
+
+// app.use(cors({
+//     origin: 'http://localhost:5002',
+//     credentials: true
+// }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5002');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 app.use('/api/genres', genresRouter);
-app.use('/api/orders', ordersRouter);
+// app.use('/api/books/:id/comments', commentsRouter);
 app.use('/api/books', booksRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/orders', ordersRouter);
 
 app.use((error, req, res, next) => {
-    // console.log(error);
+    console.log(error);
     const status = error.status || 500;
     const { message, info } = error;
     res.status(status).json({ message, info });
