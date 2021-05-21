46b3b816a7c085f009d2e30dff72543784ab2564
alchemistbg <alchemist_bg@abv.bg>
2021-05-21 21:12:14 +0200

    Change some routes
    Rename routes for register and for login
    
    Add route for deleting user

--- back-end/api/users.js
+++ back-end/api/users.js
@@ -5,11 +5,13 @@ const utils = require('../utils');
 
 const userController = require('../controllers/users');
 
-router.post('/', utils.validator.registrationDataValidator(), userController.register);
-router.post('/auth', utils.validator.loginDataValidator(), userController.login);
+router.post('/register', utils.validator.registrationDataValidator(), userController.register);
+router.post('/login', utils.validator.loginDataValidator(), userController.login);
+router.post('/logout', utils.auth, userController.logout);
 
 router.get('/:id', utils.auth, userController.getProfile);
 router.patch('/:id', utils.auth, userController.patchProfile);
+router.delete('/:id', utils.auth, userController.deleteProfile);
 
 router.get('/:id/orders', utils.auth, userController.getUserOrders);
 
