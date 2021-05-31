c1f538c2416286ec2b9d42fab6a218351af87133
alchemistbg <alchemist_bg@abv.bg>
2021-05-31 20:47:33 +0200

    Update 'auth' file
    Switch to cookies for authentication

--- back-end/utils/auth.js
+++ back-end/utils/auth.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-        token = req.headers.authorization.split('Bearer ')[1];
-    } else {
-        return res.status(401).json({
-            error: "Unauthenticated"
-        });
+    console.log(req.cookies)
 
-    }
+    const token = req.cookies['x-auth-token'] || '';
+    // if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    //     token = req.headers.authorization.split('Bearer ')[1];
+    // } else {
+    //     return res.status(401).json({
+    //         error: "Unauthenticated"
+    //     });
 
-    jwt.verify(token, 'verysecretstring', (error, decoded) => {
+    // }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decodedUser) => {
         if (error) {
-            error.status = 403;
+            // console.log(error.message);
+            error.message = 'Unauthenticated'
+            error.status = 401;
             next(error);
         }
 
-        req.user = decoded;
+        req.user = { ...decodedUser };
+        req.token = token;
         next();
     });
 
