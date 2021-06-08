f51d2b22bb0c76aea6dde4e457132ea14b863b6c
alchemistbg <alchemist_bg@abv.bg>
2021-06-08 22:03:12 +0200

    Rewrite 'RegisterForm' as a function component
    Keep the old class based version of the component

---
+++ front-end/src/components/Forms/RegisterForm-Class.jsx
@@ -0,0 +1,187 @@
+import React, { Fragment, Component, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Recaptcha from "react-google-recaptcha";
+
+import Input from "./Input";
+import { registerUser } from "../../requests/userRequests";
+import { validateForm, validateInput } from "../../utils/inputValidation";
+import { showToast } from "../../utils/helpers";
+
+class RegisterForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pageName: "Reactive Bookstore | Register",
+			registerInfo: {
+				firstName: "",
+				lastName: "",
+				email: "",
+				userName: "",
+				password: "",
+				repeatPassword: "",
+			},
+			registerErrors: {
+				userName: [],
+				email: [],
+				password: [],
+				repeatPassword: [],
+			},
+		};
+	}
+
+
+
+	handleSubmit = async (event) => {
+		const { registerInfo } = this.state;
+		event.preventDefault();
+		validateForm("register", registerInfo)
+			.then(async () => {
+				try {
+					const user = await registerUser(this.state.registerInfo);
+					showToast("success", {
+						title: `Registration successful.`,
+						message: `You will be now redirected to login page.`,
+					});
+					this.props.history.push("/login");
+				} catch (error) {
+					this.setState({
+						registerErrors: {
+							userName: [],
+							email: [],
+							password: [],
+							repeatPassword: [],
+						},
+					});
+					const registerErrors = { ...this.state.registerErrors };
+					error.response.data.info.map((err) => {
+						registerErrors[err.param].push(err.msg);
+						return undefined;
+					});
+					this.setState({
+						registerErrors,
+					});
+					showToast("error", this.state.registerErrors);
+				}
+			})
+			.catch((errors) => {
+				this.setState({
+					registerErrors: errors,
+				});
+				showToast("error", this.state.registerErrors);
+			});
+	};
+
+	handleChange = ({ currentTarget: input }) => {
+		if (input.value) {
+			input.className = "filled-input";
+		} else {
+			input.className = "";
+		}
+		const { registerInfo } = { ...this.state };
+		registerInfo[input.name] = input.value;
+		this.setState({
+			registerInfo,
+		});
+	};
+
+	render() {
+		const { pageName, registerInfo } = this.state;
+		return (
+			(document.title = pageName),
+			(
+				<Fragment>
+					<h2 className="form-header">Register</h2>
+					<div id="register-form">
+						<form
+							className="form register-form"
+							onSubmit={this.handleSubmit}
+						>
+							<Input
+								// isAutoFocus={true}
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper uname-wrapper"
+								value={registerInfo.userName}
+								onChange={this.handleChange}
+								type="text"
+								nameAndId="userName"
+								labelClassName="placeholder"
+								labelTextValue="Username"
+							/>
+							<Input
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper email-wrapper"
+								value={registerInfo.email}
+								onChange={this.handleChange}
+								type="test"
+								nameAndId="email"
+								labelClassName="placeholder"
+								labelTextValue="Email"
+							/>
+							<Input
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper fname-wrapper"
+								value={registerInfo.firstName}
+								onChange={this.handleChange}
+								type="text"
+								nameAndId="firstName"
+								labelClassName="placeholder"
+								labelTextValue="First Name"
+							/>
+							<Input
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper lname-wrapper"
+								value={registerInfo.lastName}
+								onChange={this.handleChange}
+								type="text"
+								nameAndId="lastName"
+								labelClassName="placeholder"
+								labelTextValue="Last Name"
+							/>
+							<Input
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper pass-wrapper"
+								value={registerInfo.password}
+								onChange={this.handleChange}
+								type="password"
+								nameAndId="password"
+								labelClassName="placeholder"
+								labelTextValue="Password"
+							/>
+							<Input
+								isAutoFocus={false}
+								divClassNames="form-field-wrapper pass-rep-wrapper"
+								value={registerInfo.repeatPassword}
+								onChange={this.handleChange}
+								type="password"
+								nameAndId="repeatPassword"
+								labelClassName="placeholder"
+								labelTextValue="Repeat password"
+							/>
+							<button className="form-button" type="submit">
+								Register
+							</button>
+						</form>
+						<Recaptcha
+							sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+							size='invisible'
+							ref='recaptchaRef'
+						/>
+					</div>
+					<div className="form-footer">
+						Already have an account? Please go to{" "}
+						{
+							<Link to="/login">
+								<b className="redirects">Login</b>
+							</Link>
+						}{" "}
+						page
+					</div>
+				</Fragment>
+			)
+		);
+	}
+}
+
+export default RegisterForm;
--- front-end/src/components/Forms/RegisterForm.jsx
+++ front-end/src/components/Forms/RegisterForm.jsx
@@ -1,159 +1,205 @@
-import React, { Fragment, Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, Component, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
-import Input from './Input';
-import { registerUser } from '../../services/requests';
-import { validateForm, validateInput } from '../../utils/inputValidation';
-import { showToast } from '../../utils/helpers';
+import Recaptcha from "react-google-recaptcha";
 
-class RegisterForm extends Component {
-    constructor(props) {
-        super(props);
+import Input from "./Input";
+import { registerUser } from "./../../requests/userRequests";
+import { validateForm, validateInput } from "./../../utils/inputValidation";
+import { showToast } from "./../../utils/helpers";
 
-        this.state = {
-            pageName: 'Reactive Bookstore | Register',
-            registerInfo: {
-                firstName: '',
-                lastName: '',
-                email: '',
-                userName: '',
-                password: '',
-                repeatPassword: ''
-            },
-            registerErrors: {
-                userName: [],
-                email: [],
-                password: [],
-                repeatPassword: []
-            }
-        }
-    }
+const RegisterForm = (props) => {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		userName: "",
+		password: "",
+		repeatPassword: "",
+		reCaptchaToken: ""
+	});
+	const [formErrors, setFormErrors] = useState({
+		userName: [],
+		email: [],
+		firstName: [],
+		lastName: [],
+		password: [],
+		repeatPassword: [],
+	});
 
-    handleSubmit = async (event) => {
-        const { registerInfo } = this.state;
-        event.preventDefault();
-        validateForm('register', registerInfo)
-            .then(async () => {
-                try {
-                    const user = await registerUser(this.state.registerInfo);
-                    showToast('success', {
-                        title: `Registration successful.`,
-                        message: `You will be now redirected to login page.`
-                    });
-                    this.props.history.push('/login');
-                } catch (error) {
-                    this.setState({
-                        registerErrors: {
-                            userName: [],
-                            email: [],
-                            password: [],
-                            repeatPassword: []
-                        }
-                    });
-                    const registerErrors = { ...this.state.registerErrors };
-                    error.response.data.info.map((err) => {
-                        registerErrors[err.param].push(err.msg);
-                        return undefined;
-                    });
-                    this.setState({
-                        registerErrors
-                    });
-                    showToast('error', this.state.registerErrors);
-                }
-            })
-            .catch((errors) => {
-                this.setState({
-                    registerErrors: errors
-                });
-                showToast('error', this.state.registerErrors);
-            });
+	const reCaptchaRef = useRef();
 
-    }
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		// const reCaptchaToken = await reCaptchaRef.current.executeAsync();
+		// const reCaptchaToken = await reCaptchaRef.current;
+		// reCaptchaRef.current.reset();
 
-    handleChange = ({ currentTarget: input }) => {
-        if (input.value) {
-            input.className = "filled-input";
-        }
-        else {
-            input.className = "";
-        }
-        const { registerInfo } = { ...this.state }
-        registerInfo[input.name] = input.value;
-        this.setState({
-            registerInfo
-        });
-    }
+		setFormData((formData) => ({
+			...formData
+		}));
 
-    render() {
-        const { pageName, registerInfo } = this.state;
-        return (
-            document.title = pageName,
-            <Fragment>
-                <h2 className="form-header">Register</h2>
-                <div id="register-form">
-                    <form className="form register-form" onSubmit={this.handleSubmit}>
-                        <Input
-                            // isAutoFocus={true}
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper uname-wrapper"
-                            value={registerInfo.userName}
-                            onChange={this.handleChange}
-                            type="text"
-                            nameAndId="userName"
-                            labelClassName="placeholder"
-                            labelTextValue="Username" />
-                        <Input
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper email-wrapper"
-                            value={registerInfo.email}
-                            onChange={this.handleChange}
-                            type="test"
-                            nameAndId="email"
-                            labelClassName="placeholder"
-                            labelTextValue="Email" />
-                        <Input
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper fname-wrapper"
-                            value={registerInfo.firstName}
-                            onChange={this.handleChange}
-                            type="text"
-                            nameAndId="firstName"
-                            labelClassName="placeholder"
-                            labelTextValue="First Name" />
-                        <Input
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper lname-wrapper"
-                            value={registerInfo.lastName}
-                            onChange={this.handleChange}
-                            type="text"
-                            nameAndId="lastName"
-                            labelClassName="placeholder"
-                            labelTextValue="Last Name" />
-                        <Input
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper pass-wrapper"
-                            value={registerInfo.password}
-                            onChange={this.handleChange}
-                            type="password"
-                            nameAndId="password"
-                            labelClassName="placeholder"
-                            labelTextValue="Password" />
-                        <Input
-                            isAutoFocus={false}
-                            divClassNames="form-field-wrapper pass-rep-wrapper"
-                            value={registerInfo.repeatPassword}
-                            onChange={this.handleChange}
-                            type="password"
-                            nameAndId="repeatPassword"
-                            labelClassName="placeholder"
-                            labelTextValue="Repeat password" />
-                        <button className="form-button" type="submit">Register</button>
-                    </form>
-                </div>
-                <div>Already have an account? Please go to {<Link to="/login"><b className="redirects">Login</b></Link>} page</div>
-            </Fragment>
-        );
-    }
+		validateForm("register", formData)
+			.then(async () => {
+				try {
+					// const user = await registerUser(formData, reCaptchaToken);
+					const user = await registerUser(formData);
+					showToast("success", {
+						title: `Registration successful.`,
+						message: `You will be now redirected to login page.`,
+					});
+					props.history.push("/login");
+				} catch (error) {
+					console.log(error);
+					// setFormErrors((formErrors) => ({
+					// 	...formErrors,
+					// 	userName: [],
+					// 	email: [],
+					// 	password: [],
+					// 	repeatPassword: []
+					// }));
+
+					// error.response.data.info.map((err) => {
+					// 	formErrors[err.param].push(err.msg);
+					// 	return undefined;
+					// });
+					// setFormErrors((formErrors) => ({
+					// 	...formErrors,
+					// 	userName: [],
+					// 	email: [],
+					// 	password: [],
+					// 	repeatPassword: []
+					// }));
+					// showToast("error", formErrors);
+				}
+			})
+			.catch((errors) => {
+				// console.log(errors);
+				// setFormErrors((formErrors) => ({
+				// 	...formErrors,
+				// 	...errors
+				// }));
+				// console.log(formErrors);
+				// showToast("error", formErrors);
+				showToast("error", errors);
+			});
+	}
+
+	const handleChange = ({ currentTarget: input }) => {
+		if (input.value) {
+			input.className = "filled-input";
+		} else {
+			input.className = "";
+		}
+
+		// validateInput('register', input.name, input.value)
+		// 	.then((result) => {
+		// 		console.log(result);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+
+		setFormData((formData) => ({
+			...formData,
+			[input.name]: input.value
+		}));
+	};
+
+	return (
+		(document.title = 'Reactive Bookstore | Register'),
+		(
+			<Fragment>
+				<h2 className="form-header">Register</h2>
+				<div id="register-form">
+					<form
+						className="form register-form"
+						onSubmit={handleSubmit}
+					>
+						<Input
+							// isAutoFocus={true}
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper uname-wrapper"
+							value={formData.userName}
+							onChange={handleChange}
+							type="text"
+							nameAndId="userName"
+							labelClassName="placeholder"
+							labelTextValue="Username"
+						/>
+						<Input
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper email-wrapper"
+							value={formData.email}
+							onChange={handleChange}
+							type="test"
+							nameAndId="email"
+							labelClassName="placeholder"
+							labelTextValue="Email"
+						/>
+						<Input
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper fname-wrapper"
+							value={formData.firstName}
+							onChange={handleChange}
+							type="text"
+							nameAndId="firstName"
+							labelClassName="placeholder"
+							labelTextValue="First Name"
+						/>
+						<Input
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper lname-wrapper"
+							value={formData.lastName}
+							onChange={handleChange}
+							type="text"
+							nameAndId="lastName"
+							labelClassName="placeholder"
+							labelTextValue="Last Name"
+						/>
+						<Input
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper pass-wrapper"
+							value={formData.password}
+							onChange={handleChange}
+							type="password"
+							nameAndId="password"
+							labelClassName="placeholder"
+							labelTextValue="Password"
+						/>
+						<Input
+							isAutoFocus={false}
+							divClassNames="form-field-wrapper pass-rep-wrapper"
+							value={formData.repeatPassword}
+							onChange={handleChange}
+							type="password"
+							nameAndId="repeatPassword"
+							labelClassName="placeholder"
+							labelTextValue="Repeat password"
+						/>
+						<button className="form-button" type="submit">
+							Register
+							</button>
+					</form>
+					{/* <Recaptcha
+						sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+						// size='invisible'
+						size='compact'
+						ref={reCaptchaRef}
+					/> */}
+				</div>
+				<div className="form-footer">
+					Already have an account? Please go to{" "}
+					{
+						<Link to="/login">
+							<b className="redirects">Login</b>
+						</Link>
+					}{" "}
+						page
+					</div>
+			</Fragment>
+		)
+	);
 }
 
 export default RegisterForm;
