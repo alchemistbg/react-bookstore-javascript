d581c391384407e0814520529118d759664de638
alchemistbg <alchemist_bg@abv.bg>
2021-07-04 14:23:51 +0200

    Minor code cleaning

--- client/src/components/Common/BookTable/BookTableHeader.jsx
+++ client/src/components/Common/BookTable/BookTableHeader.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const BookTableHeader = (props) => {
-    return <li className="book-list-item header">
-        <span className="book-list-item-title">
-            Book Title
-        </span>
-        <span className="book-list-item-sprice">
-            Unit price
-        </span>
-        <span className="book-list-item-qty">
-            Quantity
-        </span>
-        <span className="book-list-item-tprice">
-            Total price
-        </span>
-        {
-            props.source === "cart" ? (
-                <span className="book-list-item-delete">
-                    {/* Total price */}
-                </span>
-            ) : (null)
-        }
-    </li>
+    return <Fragment>
+        <li className={`book-list-item header ${props.source}`}>
+            <span className="book-list-item-title">
+                Book Title
+            </span>
+            <span className="book-list-item-sprice">
+                Unit price
+            </span>
+            <span className="book-list-item-qty">
+                Quantity
+            </span>
+            <span className="book-list-item-tprice">
+                Total price
+            </span>
+            {
+                props.source === "cart" ? (
+                    <span className="book-list-item-delete">
+                        {/* Total price */}
+                    </span>
+                ) : (null)
+            }
+        </li>
+    </Fragment>
 }
 
 export default BookTableHeader;
