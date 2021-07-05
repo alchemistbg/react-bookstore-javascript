130f30c69178a82ddcf1bd34fecc861ef5031e83
alchemistbg <alchemist_bg@abv.bg>
2021-07-05 21:18:14 +0200

    Reimplement 'BookCover' component
    This is done using functional component and react hooks:
    
    useState and useEffect

--- client/src/components/BookDetails/BookCover/BookCover.jsx
+++ client/src/components/BookDetails/BookCover/BookCover.jsx
@@ -1,45 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { getBookCover } from './../../../requests/bookRequests';
-import Donut from './../../../components/Common/Preloader/Donut';
+import { getBookCover } from '../../../requests/bookRequests';
+import Donut from '../../Common/Preloader/Donut';
 
-class BookCover extends Component {
-    constructor(props) {
-        super(props);
+function BookCover(props) {
 
-        this.state = {
-            isLoading: true,
-            bookCover: null
-        }
-    }
+    const [isLoading, setIsLoading] = useState(true);
+    const [bookCover, setBookCover] = useState(null);
 
-    async componentDidMount() {
-        const isbn = this.props.isbn;
-        let cover = undefined;
-        try {
-            cover = await getBookCover(isbn, 'L');
-        } catch (error) {
-            cover = 'Cover not found'
-        }
-        this.setState({
-            isLoading: false,
-            bookCover: cover
-        });
-        console.log(cover);
-    }
+    useEffect(() => {
+        getBookCover(props.isbn, 'L')
+            .then((cover) => {
+                setBookCover(cover);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
-
-    render() {
-        const { isLoading, bookCover } = this.state;
-        return (
-            isLoading ? (<Donut />) : (
-                <div className="book-cover">
-                    <img src={bookCover} alt="" />
-                    {/* <img src={bookCover} alt="" crossorigin="anonymous" /> */}
-                </div>
-            )
+    return (
+        isLoading ? (<Donut />) : (
+            <div className="book-cover">
+                <img src={bookCover} alt="" />
+            </div>
         )
-    }
+    )
 }
 
 export default BookCover;
