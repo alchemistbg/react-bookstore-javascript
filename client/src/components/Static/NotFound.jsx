e2a38986e1e5d00dde07b403381e37f62fe534ee
alchemistbg <alchemist_bg@abv.bg>
2021-07-12 21:22:20 +0200

    Add 'NotFound' component
    Add basic structure of the component
    
    Add component basic styling

--- client/src/components/Static/NotFound.jsx
+++ client/src/components/Static/NotFound.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 // import { Link } from 'react-router-dom';
 
-function NotFound() {
+function NotFound(props) {
+    const requestedUrl = props.location.pathname.slice(1);
+
     return (
         document.title = 'Page not found',
-        <h2>404 Not found!</h2>
+        <div className="not-found">
+            <div className="left">
+                <h1>404</h1>
+            </div>
+
+            <div className="right">
+                <h1>Not found!</h1>
+            </div>
+
+            <div className="middle">
+                <p>
+                    You are here, because you have requested the following non-existing resource:
+                </p>
+                <h6>{process.env.REACT_APP_BASE_URL}/{requestedUrl}</h6>
+
+            </div>
+        </div>
+
     );
 }
 export default NotFound;
