4c12a74a8990b289d4ca3b47d7c4cd6cc2ee9015
alchemistbg <alchemist_bg@abv.bg>
2021-08-04 11:58:25 +0200

    Update 'BookFromCategory' component
    Add code that change the number of items in the carusel in the landing page depending on the screen size

--- client/src/components/Home/BookFromCategory.jsx
+++ client/src/components/Home/BookFromCategory.jsx
@@ -1,28 +1,39 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useMemo } from 'react';
 
 import Slider from 'react-slick';
-import "./../../../node_modules/slick-carousel/slick/slick-theme.css";
 import "./../../../node_modules/slick-carousel/slick/slick.css";
+import "./../../../node_modules/slick-carousel/slick/slick-theme.css";
 
 import BookCard from './../BookCard/BookCard';
+import { useWindowSize } from './../../utils/hooks';
 
 import { getBooks } from './../../requests/bookRequests';
 import Donut from './../Common/Preloader/Donut';
 
-
 const BookFromCategory = (props) => {
     const { category } = props;
 
-    const sliderSettings = {
-        draggable: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        // infinite: false,
-        // centerMode: true,
-        // centerPadding: "0px"
-    }
+    const sliderSettings = useMemo(() => {
+        return {
+            draggable: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            // infinite: false,
+            // centerMode: true,
+            // centerPadding: "0px"
+        }
+    }, []);
+
+    const [pageWidth, pageHeight] = useWindowSize();
+    useEffect(() => {
+        if (pageWidth < 1025) {
+            sliderSettings.slidesToShow = 2;
+        } else if (pageWidth > 1024) {
+            sliderSettings.slidesToShow = 3;
+        }
+    }, [pageWidth, sliderSettings]);
 
     let [books, setBooks] = useState([]);
     let [isLoading, setLoading] = useState(true);
