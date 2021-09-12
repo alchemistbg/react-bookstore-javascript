67d84f604c058b34a771940baeb7ea4e4409c315
alchemistbg <alchemist_bg@abv.bg>
2021-09-12 12:09:21 +0200

    Update 'database' config file

--- config/database.js
+++ config/database.js
@@ -1,30 +1,43 @@
 const mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
+
 const Book = require('./../models/Book');
 const User = require('./../models/User');
 
-module.exports = () => {
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    });
-
-    const db = mongoose.connection;
-
-    db.once('open', (error) => {
-        if (error) {
-            console.log(error);
-        }
-
-        console.log('MongoDB ready!');
-
-        Book.seedBooks();
-        User.seedUsers();
-
-    });
-
-    db.on('error', (reason) => {
-        console.log(reason);
-    });
+const connectionSettings = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
 }
+
+module.exports.connect = async () => {
+	// await mongoose.connect("mongodb://localhost:27017/reactive", connectionSettings);
+	mongoose.connect(process.env.DATABASE_URL, connectionSettings);
+	console.log('MongoDB ready!');
+}
+
+module.exports.close = async () => {
+	await mongoose.connection.close()
+}
+
+// mongoose.connect(process.env.DATABASE_URL, connectionSettings);
+// mongoose.connect("mongodb://localhost:27017/reactive", connectionSettings);
+
+// const db = mongoose.connection;
+// console.log(db);
+// db.once('open', (error) => {
+// 	if (error) {
+// 		console.log(error);
+// 	}
+
+// 	// console.log('MongoDB ready!');
+
+// 	Book.seedBooks();
+// 	User.seedUsers();
+
+// });
+
+// db.on('error', (reason) => {
+// 	// console.log(reason);
+// });
+
+// module.exports = db;
